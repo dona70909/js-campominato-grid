cc6a920122fe0b5dc99c2c2dd317432d79c44e0b
Donatella7 <campaniellodonatella@gmail.com>
2022-03-04 16:12:26 +0100

    background color

--- js/myscript.js
+++ js/myscript.js
@@ -11,19 +11,39 @@ if (selectValue == 1){
         gridContainer.appendChild(divElement);
         divElement.classList.add("my-grid-square", "my-grid-square-one");
         divElement.innerHTML = i + 1;
+
+        divElement.addEventListener("click", function(){
+            divElement.classList.add("background-red"); 
+        });
     }
+
+
 } else if(selectValue == 2) {
     for(let i = 0; i <81; i++){
         let divElement = document.createElement("div");
         gridContainer.appendChild(divElement);
         divElement.classList.add("my-grid-square", "my-grid-square-two");
+        divElement.innerHTML = i + 1;
+
+        divElement.addEventListener("click", function(){
+            divElement.classList.add("background-blue"); 
+        });
     }
 } else if (selectValue == 3){
     for(let i = 0; i <49; i++){
         let divElement = document.createElement("div");
         gridContainer.appendChild(divElement);
         divElement.classList.add("my-grid-square", "my-grid-square-three");
+        divElement.innerHTML = i + 1;
+
+        divElement.addEventListener("click", function(){
+            divElement.classList.add("background-green"); 
+        });
     }
 } else {
     console.log("scegli");
 }
+
+
+
+

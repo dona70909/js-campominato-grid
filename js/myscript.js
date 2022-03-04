cded67da0f8bbd5f743341a3fd338a0d2088d220
Donatella7 <campaniellodonatella@gmail.com>
2022-03-04 17:22:06 +0100

    function done

--- js/myscript.js
+++ js/myscript.js
@@ -8,7 +8,7 @@ btnPlay.addEventListener("click", function(){
     let selectValue = document.getElementById("my-select").value;
     
     if (selectValue == 1){
-        for(let i = 0; i < 100; i++){
+        /*for(let i = 0; i < 100; i++){
             let divElement = document.createElement("div");
             gridContainer.appendChild(divElement);
             divElement.classList.add("my-grid-square", "my-grid-square-one");
@@ -16,12 +16,19 @@ btnPlay.addEventListener("click", function(){
             
             divElement.addEventListener("click", function(){
                 divElement.classList.add("background-red"); 
-            });
-        }
+            });  
+            
+            
+            
+            
+        } */
+    
+
+        drawBox(gridContainer,"my-grid-square","my-grid-square-one","background-red",100);
         
         
     } else if(selectValue == 2) {
-        for(let i = 0; i <81; i++){
+        /*  for(let i = 0; i <81; i++){
             let divElement = document.createElement("div");
             gridContainer.appendChild(divElement);
             divElement.classList.add("my-grid-square", "my-grid-square-two");
@@ -30,10 +37,12 @@ btnPlay.addEventListener("click", function(){
             divElement.addEventListener("click", function(){
                 divElement.classList.add("background-blue"); 
             });
-        }
+        } */
+
+        drawBox(gridContainer,"my-grid-square","my-grid-square-two","background-blue",81);
         
     } else if (selectValue == 3){
-        for(let i = 0; i <49; i++){
+        /*  for(let i = 0; i <49; i++){
             let divElement = document.createElement("div");
             gridContainer.appendChild(divElement);
             divElement.classList.add("my-grid-square", "my-grid-square-three");
@@ -42,7 +51,9 @@ btnPlay.addEventListener("click", function(){
             divElement.addEventListener("click", function(){
                 divElement.classList.add("background-green"); 
             });
-        }
+        } */
+
+        drawBox(gridContainer,"my-grid-square","my-grid-square-three","background-green",49);
         
     } else {
         console.log("scegli");
@@ -51,6 +62,15 @@ btnPlay.addEventListener("click", function(){
 });
 
 
-
-
-
+function drawBox(outsideElement,classNameOne,classNameTwo,classNameThree,N){
+    for (let i = 0; i < N ;i++){
+        let insideElement = document.createElement("div");
+        outsideElement.appendChild(insideElement);
+        insideElement.classList.add(classNameOne,classNameTwo);
+        insideElement.innerHTML = i + 1;
+        
+        insideElement.addEventListener("click", function(){
+            insideElement.classList.add(classNameThree); 
+        }); 
+    }
+}

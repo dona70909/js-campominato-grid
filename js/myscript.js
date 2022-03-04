660d4a24edb82b2d4661a0a91cb7d37207697486
Donatella7 <campaniellodonatella@gmail.com>
2022-03-04 16:37:10 +0100

    btn play

--- js/myscript.js
+++ js/myscript.js
@@ -1,48 +1,54 @@
 
-let selectValue = document.getElementById("my-select").value;
-
-const gridContainer = document.querySelector(".my-grid-container");
-
-
-
-if (selectValue == 1){
-    for(let i = 0; i < 100; i++){
-        let divElement = document.createElement("div");
-        gridContainer.appendChild(divElement);
-        divElement.classList.add("my-grid-square", "my-grid-square-one");
-        divElement.innerHTML = i + 1;
-
-        divElement.addEventListener("click", function(){
-            divElement.classList.add("background-red"); 
-        });
+const btnPlay = document.getElementById("my-btn-play");
+btnPlay.addEventListener("click", function(){
+    
+    const gridContainer = document.querySelector(".my-grid-container");
+    
+    selectValue = document.getElementById("my-select").value;
+    
+    if (selectValue == 1){
+        for(let i = 0; i < 100; i++){
+            let divElement = document.createElement("div");
+            gridContainer.appendChild(divElement);
+            divElement.classList.add("my-grid-square", "my-grid-square-one");
+            divElement.innerHTML = i + 1;
+            
+            divElement.addEventListener("click", function(){
+                divElement.classList.add("background-red"); 
+            });
+        }
+        
+        
+    } else if(selectValue == 2) {
+        for(let i = 0; i <81; i++){
+            let divElement = document.createElement("div");
+            gridContainer.appendChild(divElement);
+            divElement.classList.add("my-grid-square", "my-grid-square-two");
+            divElement.innerHTML = i + 1;
+            
+            divElement.addEventListener("click", function(){
+                divElement.classList.add("background-blue"); 
+            });
+        }
+        
+    } else if (selectValue == 3){
+        for(let i = 0; i <49; i++){
+            let divElement = document.createElement("div");
+            gridContainer.appendChild(divElement);
+            divElement.classList.add("my-grid-square", "my-grid-square-three");
+            divElement.innerHTML = i + 1;
+            
+            divElement.addEventListener("click", function(){
+                divElement.classList.add("background-green"); 
+            });
+        }
+        
+    } else {
+        console.log("scegli");
     }
+    
+});
 
-
-} else if(selectValue == 2) {
-    for(let i = 0; i <81; i++){
-        let divElement = document.createElement("div");
-        gridContainer.appendChild(divElement);
-        divElement.classList.add("my-grid-square", "my-grid-square-two");
-        divElement.innerHTML = i + 1;
-
-        divElement.addEventListener("click", function(){
-            divElement.classList.add("background-blue"); 
-        });
-    }
-} else if (selectValue == 3){
-    for(let i = 0; i <49; i++){
-        let divElement = document.createElement("div");
-        gridContainer.appendChild(divElement);
-        divElement.classList.add("my-grid-square", "my-grid-square-three");
-        divElement.innerHTML = i + 1;
-
-        divElement.addEventListener("click", function(){
-            divElement.classList.add("background-green"); 
-        });
-    }
-} else {
-    console.log("scegli");
-}
 
 
 
